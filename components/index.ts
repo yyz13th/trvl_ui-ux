a425913611acea7a9cb1cb8947a7c95dffe5fccf
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-01-31 17:31:11 +0300

    stacked exports

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export { default as Hero } from '@/components/hero.component';
+export { default as Camp } from '@/components/camp.component';
+export { default as Guide } from '@/components/guide.component';
+export { default as Features } from '@/components/features.component';
+export { default as GetApp } from '@/components/getapp.component';
+
