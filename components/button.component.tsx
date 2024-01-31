bf240c5c1200b87464f3e2522997da0618b38168
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-01-31 17:33:12 +0300

    edited properties

--- components/button.component.tsx
+++ components/button.component.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant: 'btn_dark_green' | 'btn_light_green';
+    variant: string;
 }
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
