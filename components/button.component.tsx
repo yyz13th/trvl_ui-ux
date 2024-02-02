6172360148cbb04c3f6c1fa5c906e4077b6d160b
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-02 15:50:25 +0300

    optional full property for button streching full width

--- components/button.component.tsx
+++ components/button.component.tsx
@@ -5,15 +5,17 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: string;
+    full?: boolean;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
     return (
         <button
-            className={`flexCenter ${variant} gap-3 rounded-full border`}
+            className={`flexCenter ${variant} gap-3 rounded-full border ${full && 'w-full'}`}
+
             type={type}>
             {icon && <Image src={icon} alt={title} width={24} height={24} />}
-            <label className="bold-16 whitespace-nowrap">{title}</label>
+            <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
         </button>
     )
 }
